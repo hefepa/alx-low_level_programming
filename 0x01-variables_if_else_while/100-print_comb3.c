e8935287eab997e71ab048900d8a7c2ba72372eb
hefepa <paulorimogunje@gmail.com>
2022-03-11 20:12:06 -0800

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,18 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		putchar((num1 % 10) + '0');
-		putchar((num2 % 10) + '0');
+		for (num2 = 0; num2 < 10; num2++)
+		{
 
-		if  (num1 == 9 && num2 == 9)
-			continue;
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-		putchar(',');
-		putchar(' ');
+			if  (num1 == 9 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
