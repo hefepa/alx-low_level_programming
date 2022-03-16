0a3707d8876f19ddec1108950121b94ce29dbec0
hefepa <paulorimogunje@gmail.com>
2022-03-15 23:40:25 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,14 @@
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
-	_putchar('a');
 	_putchar('u');
-	_putchar('l');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
