434621b7b45d975102612275ed5d5e5cf6009844
hefepa <paulorimogunje@gmail.com>
2022-03-17 16:50:33 -0700

    doone

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - draws a straight line in terminal
+ * Return: draw lines
+ */
+void print_line(int n);
+{
+	int c;
+
+	if (n <=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
