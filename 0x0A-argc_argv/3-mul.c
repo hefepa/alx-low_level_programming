9c0291a587ccd7dfebf5f92dc2c5f9741ab1bcba
Paul Orimogunje <paulorimogunje@gmail.com>
2022-04-01 20:15:18 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-		printf("multiplication of two number is %d\n", num1 * num2);
+		printf("%d\n", num1 * num2);
 	}
 	else
 	{
