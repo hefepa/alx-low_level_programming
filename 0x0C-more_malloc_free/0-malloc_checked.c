3b90f86858fc2a4c285537fed34a6d77a8277129
Paul Orimogunje <paulorimogunje@gmail.com>
2022-04-08 18:09:40 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - function for allocated memory for pointer
+ * @b: size of allocated memory
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more programs on malloc
